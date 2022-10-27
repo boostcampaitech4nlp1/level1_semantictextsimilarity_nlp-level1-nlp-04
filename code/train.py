c46e69c1143abff6fa9f36ce6cca4d88d7019e33
jun <jwlee9381@naver.com>
2022-10-27 01:47:33 +0000

    jun commit 1

--- code/train.py
+++ code/train.py
@@ -8,7 +8,9 @@ import transformers
 import torch
 import torchmetrics
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 
+import wandb
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, inputs, targets=[]):
@@ -57,11 +59,18 @@ class Dataloader(pl.LightningDataModule):
             text = '[SEP]'.join([item[text_column] for text_column in self.text_columns])
             outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True)
             data.append(outputs['input_ids'])
+
+        ###변경사항 2 <-> 1
+        # for idx, item in tqdm(dataframe.iterrows(), desc='tokenizing_2', total=len(dataframe)):    
+        #     text = '[SEP]'.join([item[text_column] for text_column in self.text_columns[::-1]])
+        #     outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True)
+        #     data.append(outputs['input_ids'])
+
         return data
 
     def preprocessing(self, data):
         # 안쓰는 컬럼을 삭제합니다.
-        data = data.drop(columns=self.delete_columns)
+        data = data.drop(columns=self.delete_columns) #+ data[self.target_columns].values.tolist()
 
         # 타겟 데이터가 없으면 빈 배열을 리턴합니다.
         try:
@@ -123,7 +132,7 @@ class Model(pl.LightningModule):
         self.plm = transformers.AutoModelForSequenceClassification.from_pretrained(
             pretrained_model_name_or_path=model_name, num_labels=1)
         # Loss 계산을 위해 사용될 L1Loss를 호출합니다.
-        self.loss_func = torch.nn.L1Loss()
+        self.loss_func = torch.nn.MSELoss()
 
     def forward(self, x):
         x = self.plm(x)['logits']
@@ -170,7 +179,7 @@ if __name__ == '__main__':
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='klue/roberta-small', type=str)
-    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--max_epoch', default=1, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
@@ -180,13 +189,16 @@ if __name__ == '__main__':
     parser.add_argument('--predict_path', default='/opt/ml/data/test.csv')
     args = parser.parse_args(args=[])
 
+    wandb.init(project="level1", entity="jjjjjun")
+    wandb_logger = WandbLogger(project="level1")
+
     # dataloader와 model을 생성합니다.
     dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
                             args.test_path, args.predict_path)
     model = Model(args.model_name, args.learning_rate)
 
     # gpu가 없으면 'gpus=0'을, gpu가 여러개면 'gpus=4'처럼 사용하실 gpu의 개수를 입력해주세요
-    trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1)
+    trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, logger = wandb_logger,log_every_n_steps=1)
 
     # Train part
     trainer.fit(model=model, datamodule=dataloader)
